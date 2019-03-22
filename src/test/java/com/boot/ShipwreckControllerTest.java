8748b0a3829896aeeb6caa10499f549352a1f9f8
Gaurav Sinha <gaurav.sinha1563@gmail.com>
2019-03-21 19:44:32 -0500

    Full stack complete with testing. Testing was a bonus section.

--- src/test/java/com/boot/ShipwreckControllerTest.java
+++ src/test/java/com/boot/ShipwreckControllerTest.java
@@ -9,6 +9,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -37,6 +39,7 @@ public class ShipwreckControllerTest
         Shipwreck wreck =shipwreckController.get(1L);
         verify(shipwreckRepository).findOne(1l);
 
-        assertEquals(1l, wreck.getId().longValue());
+        //assertEquals(1l, wreck.getId().longValue());
+        assertThat(wreck.getId(), is(1l));
     }
 }
